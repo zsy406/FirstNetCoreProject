6c2df920a53412333862b00a9bbe6d8a9e4d66e3
zsy406 <zsy406@users.noreply.github.com>
2018-05-18 16:36:17 +0800

    add a combin method.

--- FirstNetCoreProject1/FirstNetCoreProject1/Program.cs
+++ FirstNetCoreProject1/FirstNetCoreProject1/Program.cs
@@ -6,7 +6,23 @@ namespace FirstNetCoreProject1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dynamic dic = 56;
+            dynamic a = Combin(dic, 10);
+            Console.WriteLine(a);
+            dic = "abcd";
+            Console.WriteLine(dic);
+
+            var str = "asdf.sss.abc|045";
+            var strs = str.Split('|');
+            Console.WriteLine(strs[1]);
+            Console.ReadKey();
+
+        }
+
+
+        public static int Combin(dynamic a, int b)
+        {
+            return a.v + b;
         }
     }
 }
